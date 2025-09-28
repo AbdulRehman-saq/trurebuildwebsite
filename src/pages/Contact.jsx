5ec9d369d51211146e6c2597eaaff28ccb7b70fe
Abdul Rehman <babulsaqib010@gmail.com>
2025-09-28 22:36:19 +0500

    Updated contact form with EmailJS integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,10 +17,10 @@ const Contact = () => {
     };
 
     EmailJS.send(
-      "", //service
-      "", //template
+      "service_58vqkwr", //service
+      "template_pk29jt7", //template
       templateParams,
-      "" //public key
+      "n3EL4RrgrG7A3JP0Z" //public key
     )
     .then(
       () => {
